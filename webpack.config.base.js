6c4d1f57b90573964cb85d80af06dd950385474d
Tim Millar <heteroskedasticity99@gmail.com>
2018-12-15 13:57:56 +0000

    support class properties syntax

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -17,6 +17,9 @@ module.exports = {
           presets: [
             '@babel/preset-env',
             '@babel/preset-react'
+          ],
+          plugins: [
+            '@babel/plugin-proposal-class-properties'
           ]
         }
       }
