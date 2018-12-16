1ae97a60b1f1f72df169ef1b37dd4ab8b95e6cc0
Tim Millar <heteroskedasticity99@gmail.com>
2018-12-16 00:07:32 +0000

    set up tests to render a component (with jest & babel)

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,30 +13,11 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        options: {
-          presets: [
-            ['@babel/preset-env', {
-              targets: [
-                'last 2 versions',
-                'not dead',
-                'not < 2%',
-                'not ie 11'
-              ],
-              useBuiltIns: 'entry'
-            }],
-            '@babel/preset-react'
-          ],
-          plugins: [
-            'react-hot-loader/babel',
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-syntax-dynamic-import',
-          ]
-        }
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       }
     ]
   },
